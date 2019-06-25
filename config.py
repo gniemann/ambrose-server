8ae26798c6b29a565c8ea87e5881c6efaeea3f28
Greg Niemann <greg.niemann@gmail.com>
2019-06-25 16:13:00 -0400

    Turn off token expiration.
    At least until refresh tokens are implemented in the client.

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'devops.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = False
 
     CELERY_BROKER_URL = os.environ.get('REDIS_URL')
